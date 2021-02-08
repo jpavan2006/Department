d608244d974b113a931be5ff86a157f1e6efbd77
Pavan <jpavan2006@gmail.com>
2021-02-07 17:48:24 -0800

    Test Tag

--- src/main/java/com/pavan/department/controller/DepartmentController.java
+++ src/main/java/com/pavan/department/controller/DepartmentController.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 @Slf4j
 public class DepartmentController {
 
+
     @Autowired
     private DepartmentService departmentService;
 
--- src/main/java/com/pavan/department/repository/DepartmentRepository.java
+++ src/main/java/com/pavan/department/repository/DepartmentRepository.java
@@ -4,6 +4,7 @@ import com.pavan.department.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
